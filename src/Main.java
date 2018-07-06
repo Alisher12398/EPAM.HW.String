e46c70d5ba0829984a4519d41db43a7e238583d5
Alisher12398 <Alisher12398@gmail.com>
2018-07-07 04:04:40 +0600

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        File myFile = new File("Stih.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(myFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        StringBuffer sb = new StringBuffer();
+        while (sc.hasNext()) {
+            sb.append(sc.nextLine());
+        }
+
+        sc.close();
+
         
+
+            String[] str = sb.toString().split(" |/n|,");
+            for (String s : str){
+            System.out.println(s);
+        }
     }
 }
